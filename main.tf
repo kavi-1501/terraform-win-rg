2de2f6d1ac64b292fcb94266c57a20befb4159c5
kavi-1501 <kavitha1501@yahoo.com>
2025-08-19 00:13:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,143 +1,158 @@
 terraform {
-required_providers{ azurerm = {
-source="hashicorp/azurerm" version = "~>3.100"
-}
-}
-backend"remote"{
-hostname	="app.terraform.io" organization = "trm-kavi"
- 
-workspaces{
-name="terraform-win-rg"
-}
-}
-}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.100"
+    }
+  }
 
-provider"azurerm"{ features {}
-client_id =" 24d1d59e-6434-45a2-8dd4-9d62fa3aa9bb"
-client_secret =" 24d1d59e-6434-45a2-8dd4-9d62fa3aa9bb "
-tenant_id      = " 6babc3f8-793c-4200-9aa3-51e8d33ff572" 
-subscription_id = " 6babc3f8-793c-4200-9aa3-51e8d33ff572"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "trm-kavi"
+
+    workspaces {
+      name = "terraform-win-rg"
+    }
+  }
 }
 
-
-#Resource Group
-resource"azurerm_resource_group""rg"{ name	= "terrarg8"
-location="UKSouth"
+provider "azurerm" {
+  features {}
+  client_id       = "24d1d59e-6434-45a2-8dd4-9d62fa3aa9bb"
+  client_secret   = "24d1d59e-6434-45a2-8dd4-9d62fa3aa9bb"
+  tenant_id       = "6babc3f8-793c-4200-9aa3-51e8d33ff572"
+  subscription_id = "6babc3f8-793c-4200-9aa3-51e8d33ff572"
 }
 
-
-#VirtualNetwork
-resource"azurerm_virtual_network""vnet"{ name	= "vnet-win"
-address_space	=["10.0.0.0/16"]
-location	= azurerm_resource_group.rg.location resource_group_name=azurerm_resource_group.rg.name
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "terrarg8"
+  location = "UK South"
 }
 
-
-#Subnet
-resource"azurerm_subnet""subnet"{
- 
-name	="subnet-win"
-resource_group_name= azurerm_resource_group.rg.name virtual_network_name=azurerm_virtual_network.vnet.name address_prefixes	= ["10.0.1.0/24"]
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-win"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-
-#PublicIP(StandardSKU)
-resource"azurerm_public_ip""pip"{ name	= "winvm-pip"
-location	= azurerm_resource_group.rg.location resource_group_name=azurerm_resource_group.rg.name allocation_method= "Static"
-sku	="Standard"
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-win"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-
-# NetworkSecurityGroup
-resource"azurerm_network_security_group""nsg"{ name	= "winvm-nsg"
-location	= azurerm_resource_group.rg.location resource_group_name=azurerm_resource_group.rg.name
-
-security_rule{
-name	="Allow-RDP"
-priority	=1001
-direction="Inbound"
-access	="Allow"
-protocol= "Tcp"
-source_port_range= "*"
-destination_port_range	="3389"
-source_address_prefix	= "*"
-destination_address_prefix="*"
+# Public IP (Standard SKU)
+resource "azurerm_public_ip" "pip" {
+  name                = "winvm-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "winvm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-security_rule{
-name	="Allow-WinRM"
-priority	=1002
-direction="Inbound"
-access	="Allow"
-protocol = "Tcp"
-source_port_range = "*"
-destination_port_range	="5985"
-source_address_prefix	= "*" destination_address_prefix = "*"
-}
-}
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-
-#Subnet+NSGAssociation(ensuredependencies)
-resource"azurerm_subnet_network_security_group_association""nsg_assoc"{ subnet_id	= azurerm_subnet.subnet.id
-network_security_group_id=azurerm_network_security_group.nsg.id
-}
-
-
-#NIC
-resource"azurerm_network_interface""nic"{ name	= "winvm-nic"
-location	= azurerm_resource_group.rg.location resource_group_name=azurerm_resource_group.rg.name
-
-ip_configuration{
-name	="ipconfig1"
- 
-subnet_id	= azurerm_subnet.subnet.id private_ip_address_allocation = "Dynamic"public_ip_address_id		=azurerm_public_ip.pip.id
-}
-}
-
-
-# WindowsVM
-resource"azurerm_windows_virtual_machine""vm"{ name = "winhamd"
-resource_group_name=azurerm_resource_group.rg.name location	= azurerm_resource_group.rg.location
-size = "Standard_D4s_v3"
-admin_username = "kavi"
-admin_password="kavithal@123"
-network_interface_ids=[azurerm_network_interface.nic.id]
-
-
-os_disk{
-caching	= "ReadWrite" 
-storage_account_type="Standard_LRS"
+  security_rule {
+    name                       = "Allow-WinRM"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5985"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-
-source_image_reference{
-publisher="MicrosoftWindowsServer"
-offer	= "WindowsServer"
-sku	="2022-Datacenter"
- version= "latest"
+# Subnet + NSG Association
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "winvm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-depends_on = [ azurerm_subnet_network_security_group_association.nsg_assoc
- 
-]
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
 }
 
+# Windows Virtual Machine
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "winhamd"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D4s_v3"
+  admin_username      = "kavi"
+  admin_password      = "kavithal@123"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
 
-#WinRMExtension
-resource"azurerm_virtual_machine_extension"
-"winrm"{ name	= "enable-winrm"
-virtual_machine_id=azurerm_windows_virtual_machine.vm.id publisher	= "Microsoft.Compute"
-type	="CustomScriptExtension" type_handler_version = "1.10"
-settings=jsonencode({
-commandToExecute="winrmquickconfig-q"
-})
-depends_on = [ azurerm_windows_virtual_machine.vm
-]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+
+  depends_on = [
+    azurerm_subnet_network_security_group_association.nsg_assoc
+  ]
 }
 
+# WinRM Extension (enable)
+resource "azurerm_virtual_machine_extension" "winrm" {
+  name                 = "enable-winrm"
+  virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
 
-# Output Public IP output"vm_public_ip"{
-value=azurerm_public_ip.pip.ip_address
+  settings = jsonencode({
+    commandToExecute = "winrm quickconfig -q"
+  })
+
+  depends_on = [
+    azurerm_windows_virtual_machine.vm
+  ]
+}
+
+# Output Public IP
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
 }
